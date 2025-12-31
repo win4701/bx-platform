a604f6c510c34f6d7899b1748dc475046635c1a0
Mr Adam <cfellag1@gmail.com>
2025-12-31 19:46:44 +0100

    Delete src directory

--- src/analytics.js
+++
@@ -1,8 +0,0 @@
-import { pool } from "./db/pg.js";
-
-export async function logEvent(userId, type, meta = {}) {
-  await pool.query(
-    "INSERT INTO events(user_id,type,meta) VALUES($1,$2,$3)",
-    [userId, type, meta]
-  );
-}
--- src/db/pg.js
+++
@@ -1,7 +0,0 @@
-import pkg from "pg";
-const { Pool } = pkg;
-
-export const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false }
-});
