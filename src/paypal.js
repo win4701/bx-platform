055971f0375de669c868c6410cf10db65bc8d9bf
Mr Adam <cfellag1@gmail.com>
2025-12-31 22:37:38 +0100

    Create paypal.js

---
+++ src/paypal.js
@@ -0,0 +1,14 @@
+import paypal from "@paypal/checkout-server-sdk";
+
+const env =
+  process.env.NODE_ENV === "production"
+    ? new paypal.core.LiveEnvironment(
+        process.env.PAYPAL_CLIENT_ID,
+        process.env.PAYPAL_CLIENT_SECRET
+      )
+    : new paypal.core.SandboxEnvironment(
+        process.env.PAYPAL_CLIENT_ID,
+        process.env.PAYPAL_CLIENT_SECRET
+      );
+
+export const paypalClient = new paypal.core.PayPalHttpClient(env);
