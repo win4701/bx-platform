d51362524855cd26e2e9a7c712f40483e0675f4a
Mr Adam <cfellag1@gmail.com>
2026-01-02 18:11:57 +0100

    Delete app directory

--- app/pvp.js
+++
@@ -1,9 +0,0 @@
-// app/pvp.js
-function createMatch(bet){
-  fetch("/api/pvp/create",{method:"POST",
-    body:JSON.stringify({bet})});
-}
-function joinMatch(id){
-  fetch("/api/pvp/join",{method:"POST",
-    body:JSON.stringify({id})});
-}
