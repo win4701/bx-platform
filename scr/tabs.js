ec327edbeff4bd65d922809b98b179e1f5492443
Mr Adam <cfellag1@gmail.com>
2026-01-04 17:22:24 +0100

    Create tabs.js

---
+++ scr/tabs.js
@@ -0,0 +1,7 @@
+export const TABS = [
+  {id:'home',label:'Home'},
+  {id:'wallet',label:'Wallet'},
+  {id:'casino',label:'Casino'},
+  {id:'admin',label:'Admin'},
+  {id:'proof',label:'Proof'},
+];
