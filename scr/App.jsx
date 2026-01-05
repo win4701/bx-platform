c079d601a4f415ccd7eceb9738d2b59581875b1c
Mr Adam <cfellag1@gmail.com>
2026-01-05 18:33:05 +0100

    Update App.jsx

--- scr/App.jsx
+++ scr/App.jsx
@@ -1,39 +1,21 @@
-import {useEffect,useState} from "react";
-import {fetchState} from "./api/state";
-import Header from "./components/Header";
-import Mining from "./components/Mining";
-import Trade from "./components/Trade";
+import { Provider } from "./state";
+import NavBar from "./components/NavBar";
+import Market from "./components/Market";
 import Casino from "./components/Casino";
-import Chart from "./components/Chart";
-import Withdraw from "./components/Withdraw";
-import Leaderboard from "./components/Leaderboard";
+import Wallet from "./components/Wallet";
+import Mining from "./components/Mining";
 import Airdrop from "./components/Airdrop";
-import Tabs from "./components/Tabs";
-import "./styles/tokens.css";
 
 export default function App(){
-  const uid = window.Telegram?.WebApp?.initDataUnsafe?.user?.id || 1;
-  const [s,setS]=useState(null);
-
-  useEffect(()=>{
-    let t;
-    const load=async()=>setS(await fetchState(uid));
-    load(); t=setInterval(load,5000);
-    return ()=>clearInterval(t);
-  },[]);
-
-  if(!s) return <div>Loading…</div>;
+  const [tab,setTab]=useState("home");
   return (
-    <>
-      <Header wallet={s.wallet}/>
-      <Mining mining={s.mining}/>
-      <Trade/>
-      <Casino casino={s.casino}/>
-      <Chart value={s.wallet.bx}/>
-      <Withdraw/>
-      <Leaderboard list={s.leaderboard}/>
-      <Airdrop airdrop={s.airdrop}/>
-      <Tabs/>
-    </>
+    <Provider>
+      {tab==="home" && <Wallet/>}
+      {tab==="market" && <Market/>}
+      {tab==="casino" && <Casino/>}
+      {tab==="wallet" && <Wallet/>}
+      {tab==="airdrop" && <Airdrop/>}
+      <NavBar setTab={setTab}/>
+    </Provider>
   );
 }
