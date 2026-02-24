8784d99ee426a0ad8f918d2c533209ff9455b86b
Mr Adam <cfellag1@gmail.com>
2026-02-24 20:25:44 +0100

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -288,5 +288,4 @@ def internal_event(event: dict):
 # ======================================================
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8080))
-    app.run(host="0.0.0.0", port=port)
+    bot.infinity_polling(skip_pending=True)
