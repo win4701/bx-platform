09912f564b2230bef40dadf4e4934c4647702007
Mr Adam <cfellag1@gmail.com>
2026-02-24 23:06:20 +0100

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -287,5 +287,7 @@ def internal_event(event: dict):
 # RUN (Fly Compatible)
 # ======================================================
 
+import uvicorn
+
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=PORT)
