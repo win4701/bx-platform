1b019ad6551c47abd6b0052864fa079a49d0e61a
Mr Adam <cfellag1@gmail.com>
2026-02-24 19:44:11 +0100

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -288,5 +288,5 @@ def internal_event(event: dict):
 # ======================================================
 
 if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=PORT)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
