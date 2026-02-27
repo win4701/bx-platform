778db10e41d3684d692e3d5c48ce09730230db73
Mr Adam <cfellag1@gmail.com>
2026-02-27 21:36:31 +0100

    Update ledger.go

--- internal/ledger/ledger.go
+++ internal/ledger/ledger.go
@@ -2,6 +2,9 @@ package ledger
 
 import (
 	"database/sql"
+	"time"
+
+	"github.com/google/uuid"
 )
 
 type Service struct {
@@ -10,4 +13,16 @@ type Service struct {
 
 func New(db *sql.DB) *Service {
 	return &Service{db: db}
+}
+
+func (s *Service) Record(tx *sql.Tx, account string, amount float64) error {
+	ref := uuid.New().String()
+	ts := time.Now().Unix()
+
+	_, err := tx.Exec(`
+	INSERT INTO ledger(ref,account,debit,credit,ts)
+	VALUES (?,?,?,?,?)
+	`, ref, account, amount, 0, ts)
+
+	return err
 }
