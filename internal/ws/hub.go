6540b5acccaeeb4e050f830b9cdcc56218596e57
Mr Adam <cfellag1@gmail.com>
2026-02-27 21:58:30 +0100

    Create hub.go

---
+++ internal/ws/hub.go
@@ -0,0 +1,46 @@
+package ws
+
+import (
+	"sync"
+
+	"github.com/gofiber/websocket/v2"
+)
+
+type Hub struct {
+	clients map[*websocket.Conn]bool
+	mu      sync.Mutex
+}
+
+func NewHub() *Hub {
+	return &Hub{
+		clients: make(map[*websocket.Conn]bool),
+	}
+}
+
+func (h *Hub) Broadcast(data []byte) {
+	h.mu.Lock()
+	defer h.mu.Unlock()
+
+	for c := range h.clients {
+		c.WriteMessage(websocket.TextMessage, data)
+	}
+}
+
+func (h *Hub) Handler(c *websocket.Conn) {
+	h.mu.Lock()
+	h.clients[c] = true
+	h.mu.Unlock()
+
+	defer func() {
+		h.mu.Lock()
+		delete(h.clients, c)
+		h.mu.Unlock()
+		c.Close()
+	}()
+
+	for {
+		if _, _, err := c.ReadMessage(); err != nil {
+			break
+		}
+	}
+}
