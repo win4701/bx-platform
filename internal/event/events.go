6001d3c8c94b2891f3ba488e598fa9685d798f4e
Mr Adam <cfellag1@gmail.com>
2026-02-27 23:03:31 +0100

    Create events.go

---
+++ internal/event/events.go
@@ -0,0 +1,9 @@
+package event
+
+const (
+	EventDepositConfirmed = "deposit.confirmed"
+	EventWithdrawRequested = "withdraw.requested"
+	EventCasinoPlayed = "casino.played"
+	EventTradeExecuted = "exchange.trade"
+	EventMiningReward = "mining.reward"
+)
