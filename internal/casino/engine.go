ed926d0d6acbff01748c90ee2a6b081a3810a924
Mr Adam <cfellag1@gmail.com>
2026-02-27 22:02:35 +0100

    Update engine.go

--- internal/casino/engine.go
+++ internal/casino/engine.go
@@ -4,10 +4,19 @@ import (
 	"crypto/hmac"
 	"crypto/sha256"
 	"encoding/hex"
+	"strconv"
 )
 
-func Roll(serverSeed, clientSeed string) string {
+func GenerateRoll(serverSeed, clientSeed string, nonce int) (float64, string) {
+
 	h := hmac.New(sha256.New, []byte(serverSeed))
-	h.Write([]byte(clientSeed))
-	return hex.EncodeToString(h.Sum(nil))
+	h.Write([]byte(clientSeed + ":" + strconv.Itoa(nonce)))
+
+	hash := hex.EncodeToString(h.Sum(nil))
+
+	num, _ := strconv.ParseInt(hash[:8], 16, 64)
+
+	roll := float64(num%10000) / 100
+
+	return roll, hash
 }
