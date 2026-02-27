b8a5a474fccbee64914e1de8f755e46c6b3e64c6
Mr Adam <cfellag1@gmail.com>
2026-02-27 21:38:32 +0100

    Update service.go

--- internal/withdraw/service.go
+++ internal/withdraw/service.go
@@ -1,6 +1,9 @@
 package withdraw
 
-import "database/sql"
+import (
+	"database/sql"
+	"time"
+)
 
 type Service struct {
 	db *sql.DB
@@ -8,4 +11,20 @@ type Service struct {
 
 func New(db *sql.DB) *Service {
 	return &Service{db: db}
+}
+
+func (s *Service) Request(uid int, amount float64, address string) error {
+	_, err := s.db.Exec(`
+	INSERT INTO withdraw_queue(uid,asset,amount,address,ts)
+	VALUES (?,?,?,?,?)
+	`, uid, "usdt", amount, address, time.Now().Unix())
+
+	return err
+}
+
+func (s *Service) Approve(id int) error {
+	_, err := s.db.Exec(`
+	UPDATE withdraw_queue SET status='approved' WHERE id=?
+	`, id)
+	return err
 }
