43eb72e91883b7db9aad5b3ecfe2561a0dfe4d68
Mr Adam <cfellag1@gmail.com>
2026-02-27 21:39:08 +0100

    Update routes.go

--- internal/withdraw/routes.go
+++ internal/withdraw/routes.go
@@ -3,7 +3,22 @@ package withdraw
 import "github.com/gofiber/fiber/v2"
 
 func RegisterRoutes(app fiber.Router, service *Service) {
-	app.Get("/withdraw/test", func(c *fiber.Ctx) error {
-		return c.JSON(fiber.Map{"withdraw": "ok"})
+
+	app.Post("/withdraw/request", func(c *fiber.Ctx) error {
+		type Req struct {
+			UID     int     `json:"uid"`
+			Amount  float64 `json:"amount"`
+			Address string  `json:"address"`
+		}
+		var r Req
+		c.BodyParser(&r)
+		service.Request(r.UID, r.Amount, r.Address)
+		return c.JSON(fiber.Map{"status": "queued"})
+	})
+
+	app.Post("/withdraw/approve/:id", func(c *fiber.Ctx) error {
+		id, _ := c.ParamsInt("id")
+		service.Approve(id)
+		return c.JSON(fiber.Map{"status": "approved"})
 	})
 }
