8e8f6628a72c8e6253483f6c343d4e5c26597583
Mr Adam <cfellag1@gmail.com>
2026-03-01 19:24:43 +0100

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -309,5 +309,6 @@ if __name__ == "__main__":
     uvicorn.run(
         "backend.main:app",
         host="0.0.0.0",
-        port=8080
-                )
+        port=int(os.getenv("PORT", 8000)),
+        reload=True
+    )
