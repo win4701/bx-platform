e42e3a608c5af473857f0fccf0fd7bafa3f83dae
Mr Adam <cfellag1@gmail.com>
2026-03-14 23:18:23 +0100

    Update orderbookCache.js

--- backend/engines/orderbookCache.js
+++ backend/engines/orderbookCache.js
@@ -2,19 +2,42 @@ const cache = new Map()
 
 function setOrderbook(pair,data){
 
-cache.set(pair,data)
+cache.set(pair,{
+data,
+time:Date.now()
+})
 
 }
 
 function getOrderbook(pair){
 
-return cache.get(pair)
+const item = cache.get(pair)
+
+if(!item) return null
+
+return item.data
 
 }
 
-module.exports={
+/* CLEAN CACHE */
 
+setInterval(()=>{
+
+const now = Date.now()
+
+for(const [k,v] of cache){
+
+if(now - v.time > 30000){
+
+cache.delete(k)
+
+}
+
+}
+
+},10000)
+
+module.exports={
 setOrderbook,
 getOrderbook
-
 }
