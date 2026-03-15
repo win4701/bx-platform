40a8674ac95dad0bd6725f095493159f0dccbf4d
Mr Adam <cfellag1@gmail.com>
2026-03-15 21:59:46 +0100

    Update wsHub.js

--- backend/ws/wsHub.js
+++ backend/ws/wsHub.js
@@ -1,22 +1,30 @@
 const WebSocket = require("ws")
 
+let wss = null
+
 function startWS(server){
 
-const wss = new WebSocket.Server({ noServer:true })
-
-server.on("upgrade",(req,socket,head)=>{
-
-wss.handleUpgrade(req,socket,head,(ws)=>{
-
-wss.emit("connection",ws,req)
-
-})
-
-})
+wss = new WebSocket.Server({ server })
 
 wss.on("connection",(ws)=>{
 
 console.log("WS client connected")
+
+ws.on("message",(msg)=>{
+
+try{
+
+const data = JSON.parse(msg)
+
+handleMessage(ws,data)
+
+}catch(e){
+
+console.error("WS message error",e)
+
+}
+
+})
 
 ws.on("close",()=>{
 
@@ -26,8 +34,45 @@ console.log("WS client disconnected")
 
 })
 
-console.log("WebSocket started")
+console.log("WebSocket server started")
+
+}
+
+function broadcast(data){
+
+if(!wss) return
+
+const msg = JSON.stringify(data)
+
+wss.clients.forEach(client=>{
+
+if(client.readyState === WebSocket.OPEN){
+
+client.send(msg)
+
+}
+
+})
+
+}
+
+function handleMessage(ws,data){
+
+switch(data.type){
+
+case "ping":
+
+ws.send(JSON.stringify({type:"pong"}))
+
+break
+
+default:
+
+break
+
+}
 
 }
 
 module.exports = startWS
+module.exports.broadcast = broadcast
