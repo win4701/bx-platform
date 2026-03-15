ca8939e0ecf6ee1eb825ea6ca539fffd30808b4e
Mr Adam <cfellag1@gmail.com>
2026-03-15 02:10:17 +0100

    Update liquidityEngine.js

--- backend/engines/liquidityEngine.js
+++ backend/engines/liquidityEngine.js
@@ -1,34 +1,48 @@
 const db = require("../database")
 
-async function generateLiquidity(pair,price){
+function rand(min,max){
+return Math.random()*(max-min)+min
+}
 
-await db.query(
-`DELETE FROM market_liquidity
-WHERE pair=$1`,
-[pair]
-)
+async function generateLiquidity(){
+
+const price = 45
 
 for(let i=1;i<=10;i++){
 
-const bid = price - i * price * 0.001
-const ask = price + i * price * 0.001
+const bid = price - i*0.1
+const ask = price + i*0.1
 
-await db.query(
-`INSERT INTO market_liquidity
-(pair,side,price,amount)
-VALUES($1,'buy',$2,$3)`,
-[pair,bid,Math.random()*5]
-)
+await db.query(`
+INSERT INTO orders(user_id,pair,side,price,amount)
+VALUES(0,'BX_USDT','buy',$1,$2)
+`,[
+bid,
+rand(1,5)
+])
 
-await db.query(
-`INSERT INTO market_liquidity
-(pair,side,price,amount)
-VALUES($1,'sell',$2,$3)`,
-[pair,ask,Math.random()*5]
-)
+await db.query(`
+INSERT INTO orders(user_id,pair,side,price,amount)
+VALUES(0,'BX_USDT','sell',$1,$2)
+`,[
+ask,
+rand(1,5)
+])
 
 }
 
 }
 
-module.exports={generateLiquidity}
+function startLiquidity(){
+
+setInterval(()=>{
+
+generateLiquidity()
+
+},10000)
+
+}
+
+module.exports = {
+startLiquidity
+  }
