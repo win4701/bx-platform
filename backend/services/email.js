22cdea29702d48b829e7f4879597e730938f7cc1
Mr Adam <cfellag1@gmail.com>
2026-05-05 17:53:54 +0100

    Create email.js

---
+++ backend/services/email.js
@@ -0,0 +1,26 @@
+"use strict";
+
+const nodemailer = require("nodemailer");
+
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS
+  }
+});
+
+module.exports = {
+
+  async send(to, subject, text){
+
+    await transporter.sendMail({
+      from: process.env.EMAIL_USER,
+      to,
+      subject,
+      text
+    });
+
+  }
+
+};
