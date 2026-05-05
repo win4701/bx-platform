129713708592c44770df76b8ba6c0ce420e45853
Mr Adam <cfellag1@gmail.com>
2026-05-05 17:50:26 +0100

    Create feeEngine.js

---
+++ backend/core/feeEngine.js
@@ -0,0 +1,18 @@
+"use strict";
+
+const config = require("../config");
+
+class FeeEngine{
+
+  calculate(amount, type){
+
+    const rate = type === "maker"
+      ? config.trading.fee.maker
+      : config.trading.fee.taker;
+
+    return amount * rate;
+  }
+
+}
+
+module.exports = new FeeEngine();
