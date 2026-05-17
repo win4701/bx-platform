88a0476222e22b7f51b2fb5716b70c0dea6261e1
Mr Adam <cfellag1@gmail.com>
2026-05-17 15:03:16 +0100

    Update miningEngine.js

--- backend/engines/miningEngine.js
+++ backend/engines/miningEngine.js
@@ -1,7 +1,7 @@
 "use strict";
 
-const db = require("../database");
-const economy = require("../core/bxEconomy");
+const db = require("./database");
+const economy = require("./core/bxEconomy");
 
 /* =========================================
 CONFIG
