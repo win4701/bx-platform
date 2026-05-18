aea652d1cd9e14925b5c1214a1347edd36f6b453
Mr Adam <cfellag1@gmail.com>
2026-05-18 11:26:33 +0100

    Update tradesFeed.js

--- backend/engines/tradesFeed.js
+++ backend/engines/tradesFeed.js
@@ -8,13 +8,13 @@ const crypto =
   require("crypto");
 
 const redis =
-  require("./core/redis");
+  require("../core/redis");
 
 const ws =
-  require("./ws/wsHub");
+  require("../ws/wsHub");
 
 const candle =
-  require("./candleEngine");
+  require("../candleEngine");
 
 /* =========================================================
    CONFIG
