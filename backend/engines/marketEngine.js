273672039ba7b04452c9b74e0bcadaa2babb083b
Mr Adam <cfellag1@gmail.com>
2026-05-18 23:51:24 +0100

    Update marketEngine.js

--- backend/engines/marketEngine.js
+++ backend/engines/marketEngine.js
@@ -17,7 +17,7 @@ const ws =
   require("../ws/wsHub");
 
 const candle =
-  require("../candleEngine");
+  require("../engines/candleEngine");
 
 /* =========================================================
    CONFIG
