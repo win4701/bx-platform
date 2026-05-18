f8a13e4ed9f3c02b4c8d037aeddbed547620dc85
Mr Adam <cfellag1@gmail.com>
2026-05-18 11:24:54 +0100

    Update matchingEngine.js

--- backend/engines/matchingEngine.js
+++ backend/engines/matchingEngine.js
@@ -8,19 +8,19 @@ const crypto =
   require("crypto");
 
 const redis =
-  require("./core/redis");
+  require("../core/redis");
 
 const ledger =
-  require("./core/ledger");
+  require("../core/ledger");
 
 const ws =
-  require("./ws/wsHub");
+  require("../ws/wsHub");
 
 const candle =
-  require("./candleEngine");
+  require("../candleEngine");
 
 const orderbook =
-  require("./orderbookEngine");
+  require("../orderbookEngine");
 
 /* =========================================================
    CONFIG
